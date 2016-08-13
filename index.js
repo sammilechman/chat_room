ad114ffb1f18bcb73ac1f4f65e197d47fbdd08db
sammilechman <sammilechman@gmail.com>
2016-08-13 16:20:24 -0500

    socket.io successfully creating connections with clients

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(app.get('port'), function () {
+http.listen(app.get('port'), function () {
   console.log('Chat room app up and running!');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
 });
