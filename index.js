05fb547c74d7733470428ff4b7074ce81a78c20c
sammilechman <sammilechman@gmail.com>
2016-08-12 20:57:39 -0500

    create and serve index.html

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express();
 app.set('port', (process.env.PORT || 3000));
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.sendFile(__dirname + '/index.html');
 });
 
 app.listen(app.get('port'), function () {
